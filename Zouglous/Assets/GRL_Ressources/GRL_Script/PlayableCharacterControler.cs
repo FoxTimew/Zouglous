9c0774838c276912ee5506a14a4287b0de1661af
Mynns <g.laroumanie@rubika-edu.com>
2021-03-25 17:18:19 +0100

    camera plus contoler

---
+++ Zouglous/Assets/GRL_Ressources/GRL_Script/PlayableCharacterControler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayableCharacterControler : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody2D rigidBody;
+    [SerializeField]
+    Vector2 direction;
+    [SerializeField]
+    PlayableCharacterStat stat;
+
+    float speed;
+
+    private void Awake()
+    {
+        speed = stat.speed;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputHandler();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void InputHandler()
+    {
+        direction.y = Input.GetAxis("Vertical");
+        direction.x = Input.GetAxis("Horizontal");
+    }
+
+    void Move()
+    {
+        rigidBody.velocity = direction.normalized * Time.fixedDeltaTime * speed * 50f;
+    }
+}
---
+++ Zouglous/Assets/GRL_Ressources/GRL_Script/PlayableCharacterStat.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Playable Character Stat", menuName = "Playable Character/Stat")]
+
+public class PlayableCharacterStat : ScriptableObject
+{
+    public float speed;
+}
